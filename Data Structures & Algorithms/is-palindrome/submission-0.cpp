5c390ea440ad9651bebbaea6ac940bd05263305a
Pankaj Kumar <101548426+PankajKumar-11@users.noreply.github.com>
2026-08-05 01:56:27 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        string t = "";
+        for(char c : s){
+            if(isalnum(c)){
+                t += tolower(c);
+            }
+        }
+        string rev = t;
+        reverse(rev.begin(),rev.end());
+        return (rev==t);
+    }
+};
